c99d43e0c690e829404b0865058d83065f356dbd
Vladimir Bogatov <vladimir.bogatov@gmail.com>
2022-03-18 01:16:54 +0300

    InMemory tests fixed

--- src/main/java/ru/javawebinar/topjava/web/user/ProfileRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/user/ProfileRestController.java
@@ -2,6 +2,7 @@ package ru.javawebinar.topjava.web.user;
 
 import org.springframework.stereotype.Controller;
 import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.service.UserService;
 
 import static ru.javawebinar.topjava.web.SecurityUtil.authUserId;
 
