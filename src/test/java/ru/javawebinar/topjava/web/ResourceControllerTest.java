c5801064816a0a2b7374d351ff732b3b2a308af1
Vladimir Bogatov <vladimir.bogatov@gmail.com>
2022-03-28 23:02:49 +0300

    HW07 1.2 implemented

---
+++ src/test/java/ru/javawebinar/topjava/web/ResourceControllerTest.java
@@ -0,0 +1,19 @@
+package ru.javawebinar.topjava.web;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+
+public class ResourceControllerTest extends AbstractControllerTest {
+
+    @Test
+    void resources() throws Exception {
+        perform(get("/resources/css/style.css"))
+                .andDo(print())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.valueOf("text/css")))
+                .andExpect(status().isOk());
+    }
+}
