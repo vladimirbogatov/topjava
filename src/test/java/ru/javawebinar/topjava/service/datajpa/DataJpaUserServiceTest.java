3ec91fbebaf892a916106d40e41031456aba700d
Vladimir Bogatov <vladimir.bogatov@gmail.com>
2022-04-17 20:17:26 +0300

    Apply 10.00 patch

--- src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaUserServiceTest.java
@@ -3,7 +3,6 @@ package ru.javawebinar.topjava.service.datajpa;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.context.ActiveProfiles;
-import ru.javawebinar.topjava.UserTestData;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.service.AbstractUserServiceTest;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
@@ -15,8 +14,8 @@ import static ru.javawebinar.topjava.UserTestData.*;
 class DataJpaUserServiceTest extends AbstractUserServiceTest {
     @Test
     void getWithMeals() {
-        User admin = service.getWithMeals(ADMIN_ID);
-        USER_WITH_MEALS_MATCHER.assertMatch(admin, UserTestData.admin);
+        User actual = service.getWithMeals(ADMIN_ID);
+        USER_WITH_MEALS_MATCHER.assertMatch(actual, admin);
     }
 
     @Test
--- src/test/java/ru/javawebinar/topjava/web/json/JsonUtilTest.java
+++ src/test/java/ru/javawebinar/topjava/web/json/JsonUtilTest.java
@@ -1,14 +1,11 @@
 package ru.javawebinar.topjava.web.json;
 
 import org.junit.jupiter.api.Test;
-import ru.javawebinar.topjava.MealTestData;
 import ru.javawebinar.topjava.model.Meal;
 
 import java.util.List;
 
-import static ru.javawebinar.topjava.MealTestData.MEAL_MATCHER;
-import static ru.javawebinar.topjava.MealTestData.adminMeal1;
-import static ru.javawebinar.topjava.MealTestData.meals;
+import static ru.javawebinar.topjava.MealTestData.*;
 
 class JsonUtilTest {
 
@@ -24,7 +21,7 @@ class JsonUtilTest {
     void readWriteValues() {
         String json = JsonUtil.writeValue(meals);
         System.out.println(json);
-        List<Meal> meals = JsonUtil.readValues(json, Meal.class);
-        MEAL_MATCHER.assertMatch(meals, MealTestData.meals);
+        List<Meal> actual = JsonUtil.readValues(json, Meal.class);
+        MEAL_MATCHER.assertMatch(actual, meals);
     }
 }
